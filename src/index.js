6d8b7251d1c817cf5e2177ce4dbdd15a8f0d5e48
amanmibra <amanmibra@gmail.com>
2019-02-25 00:04:42 -0500

    Step 1: Create React App and Fix Directories

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// containers
+import App from './containers/App';
+
+// styles
+import './styles/index.css';
+
+// utils
+import * as serviceWorker from './utils/serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
